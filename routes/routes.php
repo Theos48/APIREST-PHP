71a7f0ad3ea51455c3635dff26c2cf4d8bdb6b0e
David Ramirez <David_Ramirezz@hotmail.com>
2022-05-18 15:36:43 -0500

    Construccion de rutas

--- routes/routes.php
+++ routes/routes.php
@@ -12,16 +12,30 @@ if (count($routesArray) == 0) {
   return;
 }
 
-
-
-$response['detalles'] = "Estoy en {$routesArray[1]}";
-
-echo json_encode($routesArray, true);
-
-// switch ($routesArray[1]){
-//   case 'cursos':
-    
-//     break;
-//   case 'registros':
-//     break;
-// }
+//Comprobación de ruta
+$second_parameter_check = !empty($routesArray[2]);
+switch ($routesArray[1]) {
+  case 'cursos':
+    //Verifición de segundo parametro
+    if ($second_parameter_check && $routesArray[1] == 'cursos') {
+      $response['detalles'] = "Estoy en el curso {$routesArray[2]}";
+      echo json_encode($response, true);
+      return;
+    }
+    // En caso de un solo parametro
+    $response['detalles'] = "Estoy en {$routesArray[1]}";
+    echo  json_encode($response, true);
+    break;
+  case 'registros':
+    if ($second_parameter_check && $routesArray[1] == 'registros') {
+      $response['detalles'] = "Estoy en el registro {$routesArray[2]}";
+      echo json_encode($response, true);
+      return;
+    }
+    $response['detalles'] = "Estoy en {$routesArray[1]}";
+    echo  json_encode($response, true);
+    break;
+  default:
+    echo  json_encode($response, true);
+    break;
+}
